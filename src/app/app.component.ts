d9350f6dba5970e7e39f2acd8a9023982dc45159
Matheus <matheuznardi@gmail.com>
2024-12-18 14:56:35 -0300

    feat: uso de deferrable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NewComponent } from './components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TempalteVariablesComponent } from './components/template/tempalte-variables/tempalte-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewComponent, TemplateBindingComponent, TempalteVariablesComponent, TemplateControlFlowComponent],
+  imports: [RouterOutlet, NewComponent, TemplateBindingComponent, TempalteVariablesComponent, TemplateControlFlowComponent, TemplateDeferrableViewsComponent],
   styles: [`
     h1{
       color: purple;
@@ -20,7 +21,8 @@ import { TemplateControlFlowComponent } from './components/template/template-con
   
   <!-- <app-template-binding/>
   <app-tempalte-variables/> -->
-  <app-template-control-flow/>
+  <!-- <app-template-control-flow/> -->
+   <app-template-deferrable-views/>
   ` ,
 })
 export class AppComponent {
